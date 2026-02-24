6328ae2cd528325829f773f569d655f30e98673c
Yassir Jaber <168913095+Y1ssir@users.noreply.github.com>
2026-02-24 11:01:26 +0000

    Refactor Category class by removing comments
    Removed comments and adjusted formatting in Category class.

--- tp1/monolith/src/main/java/com/ecommerce/monolith/category/model/Category.java
+++ tp1/monolith/src/main/java/com/ecommerce/monolith/category/model/Category.java
@@ -5,17 +5,14 @@ import java.util.List;
 import com.ecommerce.monolith.product.model.Product;
 
 @Entity
-@Data // Génère les Getters/Setters grâce à Lombok
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
-    // Cette ligne permet de voir tous les produits d'une catégorie
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 }
